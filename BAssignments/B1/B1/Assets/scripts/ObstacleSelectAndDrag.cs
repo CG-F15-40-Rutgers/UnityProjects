a3718937d10de6078fe3745f320b780704fd8929
Maxine <maxine.deines@gmail.com>
2015-10-17 23:50:54 -0400

    moveable obstacles (needs to be intergrated with agent movement) moving obstacles (can alter the speed, direction, and distance of these obstacles)

---
+++ BAssignments/B1/B1/Assets/scripts/ObstacleSelectAndDrag.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleSelectAndDrag : MonoBehaviour {
+
+	//NEED INFO ABOUT IF OBJECTS ARE CURRENTLY SELECTED FOR AGENTS SO WE CAN INTERCHANGE SELECT/DESELECT
+	public float speed = 1.0f;
+
+	private Transform objSelected;
+	private bool selected;
+	private Color startcolor;
+
+	void Start(){
+		objSelected = null;
+		selected = false;
+	}
+
+	void FixedUpdate () {
+		if (Input.GetButtonDown ("Fire1")) {  
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);     
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, 200) && (hit.collider.tag == "obstacle")) {
+				if(objSelected!=null&&hit.transform!=objSelected)
+					Deselect ();
+				Select(hit.transform);
+			}else{
+				Debug.Log(objSelected);
+				if(objSelected!=null)
+					Deselect ();
+			}
+		}
+
+		if (selected) {
+			Navigate();
+		}
+
+	}
+
+	void Select(Transform obj){
+		Debug.Log ("boop");
+		selected = true;
+		objSelected = obj;
+		startcolor = obj.gameObject.GetComponent<Renderer>().material.color;
+		obj.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+	}
+
+	void Deselect(){
+		Debug.Log (startcolor);
+		objSelected.gameObject.GetComponent<Renderer>().material.color = startcolor;
+		selected = false;
+		objSelected = null;
+
+	}
+
+	void Navigate(){
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical"));
+		objSelected.position += move * speed * Time.deltaTime;
+
+	}
+}
---
+++ BAssignments/B1/B1/Assets/scripts/Rotator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public enum Movement{x,z};
+	public Movement movement;
+	public float distance=4.0f;
+	public float speed=1.0f;
+
+	private Vector3 initialPosition;
+	private bool positiveDirection;
+
+	void Start () {
+		initialPosition = transform.position;
+		positiveDirection = true;
+	}
+	
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		
+		if(Mathf.Abs(transform.position.x-initialPosition.x)>distance||Mathf.Abs(transform.position.z-initialPosition.z)>distance){
+			if(positiveDirection)
+				positiveDirection=false;
+			else positiveDirection=true;
+		}
+		Move ();
+
+	}
+
+	void Move(){
+		if (movement == Movement.x) {
+			if(positiveDirection){
+				transform.Translate( new Vector3(1.0f,0,0.0f)*speed*Time.deltaTime,Space.World);
+			}else{
+				transform.Translate( new Vector3(-1.0f,0,0.0f)*speed*Time.deltaTime,Space.World);
+			}
+		} else {
+			if(positiveDirection){
+				transform.Translate( new Vector3(0.0f,0,1.0f)*speed*Time.deltaTime,Space.World);
+			}
+			else{
+				transform.Translate( new Vector3(0.0f,0,-1.0f)*speed*Time.deltaTime,Space.World);
+			}
+		}
+	}
+}
