8493696e435e37318c4981ab682d474bc563af35
RoVernekar <ro.vernekar@gmail.com>
2015-10-19 21:36:54 -0400

    Added main menu

---
+++ BAssignments/B1/B1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public int level;
+
+	void OnMouseUp () {
+        Application.LoadLevel(level);
+	}
+
+}
